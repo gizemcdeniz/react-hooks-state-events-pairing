7a5b4e84f3773e4e1bc25d1e9ca457d8ad5221bc
gizem <gizemc.deniz@gmail.com>
2022-02-20 23:42:28 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
+
 import video from "../data/video.js";
+import '../index.css';
 
 function App() {
-  console.log("Here's your data:", video);
+console.log("Here's your data:", video);
 
   return (
+    <>
+    <div className="center">
     <div className="App">
       <iframe
         width="919"
@@ -14,6 +18,18 @@ function App() {
         title="Thinking in React"
       />
     </div>
+    <div className="commentsSection">
+    {/* {Object.keys(video).map(videos => <p key={video.id[0]}>{video.title}</p>)} */}
+   <h1>{video.title}</h1>
+   <p>{video.views} Views | Uploaded {video.createdAt}</p>
+   <button>{video.upvotes} &#128077; </button>
+   <button>{video.downvotes} &#128077; </button><br></br>
+   <button>Hide Comments</button>
+   <hr></hr>
+       {video.comments.map(comment => <p key={comment.id}>{comment.user}<br></br>{comment.comment}</p>)}
+       </div>
+       </div>
+    </>
   );
 }
 
